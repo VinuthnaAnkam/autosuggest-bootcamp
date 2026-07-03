1bf555c7cf9ba5ab1cd40172d2e8c791f9892ff6
Vinuthna Ankam <24wh1a05k1@bvrithyderabad.edu.in>
2026-07-03 13:42:22 +0530

    server changes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express =require('express')
+
 const app=express()
-const PORT=3000;
+
+const PORT=process.env.PORT||3000;
+//web server:
+
 app.use(express.static("Frontend"))
+
 app.listen(PORT,function(){
     console.log("Successfully running at http://localhost:"+PORT)
 })
